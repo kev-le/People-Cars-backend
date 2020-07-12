10e6a299f6925ed5f4b5bfbd062aadeb846e5e77
Kevin Le <kevin_le@sfu.ca>
2020-07-11 19:00:22 -0700

    CORS configuration

--- config/application.rb
+++ config/application.rb
@@ -17,3 +17,10 @@ module Artona
     # the framework and any gems in your application.
   end
 end
+
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins '*'
+    resource '*', headers: :any, methods: [:get, :post, :patch, :put]
+  end
+end
