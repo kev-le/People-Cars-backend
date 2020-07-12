51c785041064b8413615c5632839f3c2ea8fb67e
Kevin Le <kevin_le@sfu.ca>
2020-07-11 17:10:11 -0700

    Add CRUD operations for person controller

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -1,4 +1,39 @@
 class PeopleController < ApplicationController
-    def new
+
+    # skipping CSRF verification
+    skip_before_action :verify_authenticity_token
+
+    def index
+        @people = Person.all
+        render json: @people
     end
+
+    def show
+        @person = Person.find(params[:id])
+        render json: @person
+    end
+
+    def destroy
+        @person = Person.find(params[:id])
+        @person.destroy
+    end
+
+    def create
+        @person = Person.new(person_params)
+        @person.save
+
+        render json: @person
+    end
+
+    def update
+        @person = Person.find(params[:id])
+        @person.update(person_params)
+
+        render json: @person
+    end
+
+    private
+        def person_params
+            params.require(:person).permit(:firstName, :lastName, :email)
+        end
 end
--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,3 @@
 class Person < ApplicationRecord
-    has_many :cars
+    has_many :cars, dependent: :delete_all
 end
